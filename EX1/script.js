eb73062e6cd9061707bdd24d489afb0e0ed7c7df
DiepvensBent <bent.diepvens@student.ehb.be>
2021-10-04 21:33:08 +0200

    ex7 it works

--- EX1/script.js
+++ EX1/script.js
@@ -1,63 +1,77 @@
 "Use Strict";
+let price = 0;
+let dishes = [];
+dishes.push({
+    id: '1',
+    name: 'Burger and french fries',
+    price: 10
+}, {
+    id: '2',
+    name: 'Smos',
+    price: 3
+}, {
+    id: '3',
+    name: 'Bitterbal',
+    price: 1
+}, {
+    id: '4',
+    name: 'Frikandel',
+    price: 2
+}, {
+    id: '5',
+    name: 'Pizza',
+    price: 8
+});
 
 window.onload = function () {
     console.log("loaded");
-    let dishes = [];
-    dishes.push({
-        id: '1',
-        name: 'Burger and french fries',
-        price: 10
-    }, {
-        id: '2',
-        name: 'Smos',
-        price: 3
-    }, {
-        id: '3',
-        name: 'Bitterbal',
-        price: 1
-    }, {
-        id: '4',
-        name: 'Frikandel',
-        price: 2
-    }, {
-        id: '5',
-        name: 'Pizza',
-        price: 8
-    });
-
     dishes.forEach(element => {
         let radio = `<input type="checkbox" id="${element.name}" name="options">
         <label for="${element.id}">${element.name}</label>`
-        console.log(radio);
         document.getElementById('options').insertAdjacentHTML("afterend", radio);
     });
 
     document.getElementById("form").addEventListener('submit', event => {
         event.preventDefault();
-        let orderDetails = {
-            name: "",
-            email: "",
-            order: ""
-        }
-        orderDetails.name = document.getElementById('nameInput').value;
-        orderDetails.email = document.getElementById('emailInput').value;
+        printOrder();
+    });
 
-        let price = 0;
-        dishes.forEach(element => {
-            let isChecked = document.getElementById(element.name).checked;
-            if (isChecked) {
-                price = price + element.price;
-            }
-        });
-        console.log(price);
-        let selectedOption = document.querySelector('input[name=options]:checked');
-        orderDetails.order = selectedOption.id;
-
-        printOrder(orderDetails);
+    document.getElementById("calculateButton").addEventListener('submit', event => {
+        event.preventDefault();
+        calculatePrice();
     });
 };
 
-function printOrder(orderDetails) {
-    let bericht = `<p>The order for the customer ${orderDetails.name} is the following:  ${orderDetails.order}. The customer may be notified by email: ${orderDetails.email}</p>`
-    document.getElementById('message').innerHTML = bericht;
-}
+function printOrder() {
+    calculatePrice();
+    console.log(price);
+
+    let orderDetails = {
+        name: "",
+        email: "",
+        order: []
+    }
+    orderDetails.name = document.getElementById('nameInput').value;
+    orderDetails.email = document.getElementById('emailInput').value;
+    dishes.forEach(element => {
+        let isChecked = document.getElementById(element.name).checked;
+        if (isChecked) {
+            orderDetails.order.push(element.name);
+        }
+    });
+    let orderMessage = `<p>The order for the customer ${orderDetails.name} is the following:  ${orderDetails.order}. The customer may be notified by email: ${orderDetails.email}. The order will cost ${price} euro.</p>`
+    document.getElementById('message').innerHTML = orderMessage;
+};
+
+function calculatePrice() {
+    price = 0;
+    dishes.forEach(element => {
+        let isChecked = document.getElementById(element.name).checked;
+        if (isChecked) {
+            price = price + element.price;
+        };
+    });
+    console.log(price);
+    let priceMessage = `<h1>Price: ${price} euro.</h1>`;
+    document.getElementById('priceText').innerHTML = priceMessage;
+};
