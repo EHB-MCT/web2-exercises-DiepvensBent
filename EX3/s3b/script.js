3bc6ca83ff91bf3223cf722a6183540deeaa8499
DiepvensBent <71712367+DiepvensBent@users.noreply.github.com>
2021-10-18 19:43:02 +0200

    exercise 3B displays search

--- EX3/s3b/script.js
+++ EX3/s3b/script.js
@@ -3,6 +3,7 @@ let baseUrl = 'http://www.omdbapi.com/?apikey=cdd80856';
 window.onload = function () {
     document.getElementById('searchform').addEventListener('submit', event => {
         event.preventDefault();
+        document.getElementById('containerID').innerHTML = null;
         let searchedTerm = document.getElementById('inputTitle').value;
         console.log(searchedTerm);
         let url = baseUrl + `&s=${searchedTerm}`;
@@ -10,6 +11,22 @@ window.onload = function () {
             let resultingData = result.Search;
             console.log(resultingData);
             for (let el of resultingData) {
+                let card = `<div class="card mb-3" style="max-width: 540px;">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                        <img src="${el.Poster}" class="card-img" alt="...">
+                    </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">${el.Title}</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                                    additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small class="text-muted">$el.Year</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>`
+                document.getElementById('containerID').insertAdjacentHTML("beforeend", card);
                 console.log(el);
                 console.log(el.Poster);
                 console.log(el.Title);
